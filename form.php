d3f48738043a159d137f036d1b1f16b8017ced0a
Zanhua1999 <skyair886@gmail.com>
2019-12-04 11:48:14 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Email</title>
+</head>
+<body>
+    <form action="#" method="#" >
+    <input type="text" placeholder="Voer je E-mail adres in">
+    <input type="submit" value="Verstuur">
+    </form>
+</body>
+</html>
+
+<?php
+
+?>
